dbcc3fd904b9c603bc236c9430a2b5344c862dab
Diego Nieto <diego.nieto.m@outlook.com>
2022-12-06 19:48:44 +0100

    costs: fix error when adding costs csv to the ddbb
    Fixes #18

--- ddbbfeeder.py
+++ ddbbfeeder.py
@@ -5,11 +5,21 @@ import os
 import logging
 
 class DatabaseFeeder(Database):
-    def _feedAccountWithFileDb(self, inputFile):
-        print('Trying to read file: ' + inputFile)
+    _logger = None
+
+    def __init__(self) -> None:
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._logger.setLevel(logging.DEBUG)
+        super().__init__()
+
+    def _initFeedTable(self, inputFile):
+        self._logger.debug('Trying to read file: ' + inputFile)
         self._reader = CSVReader()
         self._reader.read(inputFile)
-        data = self._reader.getData()
+        return self._reader.getData()
+
+    def _feedAccountWithFileDb(self, inputFile):
+        data = self._initFeedTable(inputFile)
 
         # model: {date, date, desc, price, balance}
         for line in data:
@@ -20,40 +30,37 @@ class DatabaseFeeder(Database):
                 lineDesc = line[2].replace('\'',' ')
                 values = lineDate1 + ',' + lineDate2 + ',' + '\'' + lineDesc + '\'' + ',' + self.toMoney(line[3]) + ',' + self.toMoney(line[4])
                 self._ddbb.insertData('account', values)
-                print(values)
 
     def _feedMovementsWithFileDb(self, inputFile):
-        logging.debug('Trying to read file: ' + inputFile)
-        self._reader = CSVReader()
-        self._reader.read(inputFile)
-        data = self._reader.getData()
+        data = self._initFeedTable(inputFile)
 
         # model: {date, desc, price}
         for line in data:
-            dateValue = re.search("\d\d\/\d\d\/.*", line[0])
+            dateValue = re.search("\d\d\d\d\-\d\d\-.*", line[0])
+            self._logger.error(line[0])
             if dateValue is not None:
-                lineDate = self.parseDate(line[0])
+                lineDate = self.toJulianday(line[0])
                 lineDesc = line[1].replace('\'',' ')
                 values = lineDate + ',' + '\'' + lineDesc + '\'' + ',' + self.toMoney(line[3])
                 self._ddbb.insertData('costs', values)
-                print(values)
 
     def importDataFromDirectory(self, path, type):
         if type is None:
-            logging.error('Data type to import not defined')
+            self._logger.error('Data type to import not defined')
             raise Exception
         if type == 'costs':
             self._importFilesInPath(path, self._feedMovementsWithFileDb)
         elif type == 'account':
             self._importFilesInPath(path, self._feedAccountWithFileDb)
         else:
-            logging.error('Data type not defined')
+            self._logger.error('Data type not defined')
             raise Exception
 
     def _importFilesInPath(self, path, feedFunction):
+        self._logger.debug('Checking directory ' + path)
         dataFiles = os.listdir(path)
         for file in dataFiles:
             if file.endswith('.csv'):
                 filepath = os.path.join(path, file)
-                logging.debug('Trying to import file: ' + filepath)
+                self._logger.debug('Trying to import file: ' + filepath)
                 feedFunction(filepath)
